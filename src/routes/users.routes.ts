fc08674854499bafe4579ee6b553389facb9e55b
RossiniM <rossini1408@gmail.com>
2020-08-05 12:11:03 -0300

    - add service to create user - add user's route - add bcrypt to hash password

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,19 @@
+import { Router, Request, Response } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request: Request, response: Response) => {
+  try {
+    const { name, email, password } = request.body;
+    const userService = new CreateUserService();
+    const user = await userService.execute({ name, email, password });
+    delete user.password;
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { startOfHour } from 'date-fns';
+import { getRepository } from 'typeorm';
+import { hash } from 'bcrypt';
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+    const checkUserExists = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUserExists) throw Error('There is already user with same email');
+
+    const hashedPassword = await hash(password, 8);
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
