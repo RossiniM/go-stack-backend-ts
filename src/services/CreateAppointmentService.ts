ad3ee59bd83391848cc4320c490c63801bfb7de4
RossiniM <rossini1408@gmail.com>
2020-08-05 12:09:20 -0300

    - add migration which create relation between appointment and user - update routes,service due to new relationship

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,12 +4,12 @@ import Appointment from '../models/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 interface Request {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ provider, date }: Request): Promise<Appointment> {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
     const appointmentRepository = getCustomRepository(AppointmentRepository);
     const appointmentDate = startOfHour(date);
     const isThereAppointmentInSameDate = await appointmentRepository.findByDate(
@@ -20,7 +20,7 @@ class CreateAppointmentService {
       throw Error('There is already appointment with same date');
 
     const appointment = appointmentRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 
